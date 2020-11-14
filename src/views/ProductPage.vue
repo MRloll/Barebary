3e3ad368c1991818b3a58a97ac0f89da71be247e
MRloll <walidelloll2018@yahoo.com>
2020-11-14 14:55:17 +0200

    sending product obj to product page

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,7 +1,28 @@
 <template>
-    <div>
-        <h1>{{ this.$route.params.name }}</h1>
+    <div class="custom-container">
+        <main class="porduct-show">
+            <div class="row">
+                <div class="col">
+                    <div class="slider">
+                        s
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="description">
+                        s
+                    </div>
+                </div>
+            </div>
+        </main>
+        <h1>{{ product.name }}</h1>
     </div>
 </template>
 
+<script>
+export default {
+    props: ["product"]
+};
+</script>
+
 <style lang="scss" scoped></style>
+s
